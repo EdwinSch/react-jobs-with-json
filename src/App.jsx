b50f5be88f09966137c93b833dfd857abd86de5a
EdwinSch <edwinschutjes@gmail.com>
2024-03-19 16:33:48 +0100

    homepage setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllBtn from "./components/ViewAllBtn";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllBtn />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/JobListings.jsx
@@ -0,0 +1,24 @@
+import jobsData from "../jobs.json";
+import Job from "./Job";
+
+const JobListings = () => {
+  const recentJobs = jobsData.slice(0, 3);
+
+  return (
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          Browse Jobs
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {/* Map Jobs*/}
+          {recentJobs.map((job) => {
+            return <Job key={job.id} {...job} />;
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default JobListings;
