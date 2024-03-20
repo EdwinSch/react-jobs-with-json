7dd271f12ff7a9a0f36b4ebcf41f81c7aebe1618
EdwinSch <edwinschutjes@gmail.com>
2024-03-20 16:47:44 +0100

    add job functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import MainLayout from "./pages/MainLayout";
 import Homepage from "./pages/Homepage";
 import JobsPage from "./pages/JobsPage";
 import SingleJobPage, { jobLoader } from "./pages/SingleJobPage";
+import AddJobPage from "./pages/AddJobPage";
 import NotFound from "./pages/NotFound";
 
 import {
@@ -11,18 +12,27 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<Homepage />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      <Route path="/jobs/:id" element={<SingleJobPage />} loader={jobLoader} />
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
-);
+const addJob = (newJob) => {
+  console.log(newJob);
+};
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Homepage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route
+          path="/jobs/:id"
+          element={<SingleJobPage />}
+          loader={jobLoader}
+        />
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    )
+  );
+
   return <RouterProvider router={router} />;
 }
 
---
+++ src/pages/AddJobPage.jsx
@@ -0,0 +1,244 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AddJobPage = ({ addJobSubmit }) => {
+  // Init Navigate from Router
+  const navigate = useNavigate();
+
+  const [input, setInput] = useState({
+    title: "",
+    type: "Full-Time",
+    location: "",
+    description: "",
+    salary: "Under $50K",
+    companyName: "",
+    companyDescription: "",
+    contactEmail: "",
+    contactPhone: "",
+  });
+
+  // Handle input change
+  const handleChange = (event) => {
+    setInput({
+      ...input,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Structure inputs as JSON object
+    const newJob = {
+      title: input.title,
+      type: input.type,
+      location: input.location,
+      description: input.description,
+      salary: input.salary,
+      company: {
+        name: input.companyName,
+        description: input.companyDescription,
+        contactEmail: input.contactEmail,
+        contactPhone: input.contactPhone,
+      },
+    };
+
+    // console.log(newJob);
+    addJobSubmit(newJob);
+    return navigate("/jobs");
+  };
+
+  return (
+    <section className="bg-indigo-50">
+      <div className="container m-auto max-w-2xl py-24">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <form onSubmit={handleSubmit}>
+            <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
+
+            <div className="mb-4">
+              <label
+                htmlFor="type"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Job Type
+              </label>
+              <select
+                required
+                id="type"
+                name="type"
+                value={input.type}
+                onChange={handleChange}
+                className="border rounded w-full py-2 px-3"
+              >
+                <option value="Full-Time">Full-Time</option>
+                <option value="Part-Time">Part-Time</option>
+                <option value="Remote">Remote</option>
+                <option value="Internship">Internship</option>
+              </select>
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2">
+                Job Listing Name
+              </label>
+              <input
+                type="text"
+                id="title"
+                name="title"
+                value={input.title}
+                onChange={handleChange}
+                className="border rounded w-full py-2 px-3 mb-2"
+                placeholder="eg. Beautiful Apartment In Miami"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="description"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Description
+              </label>
+              <textarea
+                id="description"
+                name="description"
+                value={input.description}
+                onChange={handleChange}
+                className="border rounded w-full py-2 px-3"
+                rows="4"
+                placeholder="Add any job duties, expectations, requirements, etc"
+              ></textarea>
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="type"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Salary
+              </label>
+              <select
+                id="salary"
+                name="salary"
+                value={input.salary}
+                onChange={handleChange}
+                className="border rounded w-full py-2 px-3"
+                required
+              >
+                <option value="Under $50K">Under $50K</option>
+                <option value="$50K - 60K">$50K - $60K</option>
+                <option value="$60K - 70K">$60K - $70K</option>
+                <option value="$70K - 80K">$70K - $80K</option>
+                <option value="$80K - 90K">$80K - $90K</option>
+                <option value="$90K - 100K">$90K - $100K</option>
+                <option value="$100K - 125K">$100K - $125K</option>
+                <option value="$125K - 150K">$125K - $150K</option>
+                <option value="$150K - 175K">$150K - $175K</option>
+                <option value="$175K - 200K">$175K - $200K</option>
+                <option value="Over $200K">Over $200K</option>
+              </select>
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2">
+                Location
+              </label>
+              <input
+                type="text"
+                id="location"
+                name="location"
+                value={input.location}
+                onChange={handleChange}
+                className="border rounded w-full py-2 px-3 mb-2"
+                placeholder="Company Location"
+              />
+            </div>
+
+            <h3 className="text-2xl mb-5">Company Info</h3>
+
+            <div className="mb-4">
+              <label
+                htmlFor="company"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Company Name
+              </label>
+              <input
+                type="text"
+                id="company"
+                name="companyName"
+                value={input.companyName}
+                onChange={handleChange}
+                className="border rounded w-full py-2 px-3"
+                placeholder="Company Name"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="company_description"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Company Description
+              </label>
+              <textarea
+                id="company_description"
+                name="companyDescription"
+                value={input.companyDescription}
+                onChange={handleChange}
+                className="border rounded w-full py-2 px-3"
+                rows="4"
+                placeholder="What does your company do?"
+              ></textarea>
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="contact_email"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Contact Email
+              </label>
+              <input
+                type="email"
+                id="contact_email"
+                name="contactEmail"
+                value={input.contactEmail}
+                onChange={handleChange}
+                className="border rounded w-full py-2 px-3"
+                placeholder="Email address for applicants"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="contact_phone"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Contact Phone
+              </label>
+              <input
+                type="tel"
+                id="contact_phone"
+                name="contactPhone"
+                value={input.contactPhone}
+                onChange={handleChange}
+                className="border rounded w-full py-2 px-3"
+                placeholder="Optional phone for applicants"
+              />
+            </div>
+
+            <div>
+              <button
+                className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Add Job
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AddJobPage;
